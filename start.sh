339cb5c6dab5c5c9eb216b0d635d47c044f9843d
muraro88 <123836926+muraro88@users.noreply.github.com>
2025-08-06 09:24:26 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -14,6 +14,6 @@ bundle exec rails db:migrate
 
 multirun \
     "bundle exec sidekiq -C config/sidekiq.yml" \
-    "bundle exec rails s -b 0.0.0.0 -p $PORT"
+    "bundle exec rails s -b '::' -p $PORT"
 
 false
